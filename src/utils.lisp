19200ad2db330e49e61f8ef92c6bdd2d39aac4b9
Mikhail Novikov <freiksenet@gmail.com>
2012-08-21 23:18:11 +0300

    This looks much lispier.

--- src/utils.lisp
+++ src/utils.lisp
@@ -2,14 +2,29 @@
   (:use #:cl #:alexandria)
   (:export
    #:make-32-array
-   #:rsh))
+   #:rsh
+   #:range))
 (cl:in-package #:persistine.utils)
 
+(declaim (inline make-32-array))
 (defun make-32-array ()
   (make-array '(32)
               :initial-element nil
               :fill-pointer 0))
 
+(declaim (inline rsh))
 (defun rsh (integer count)
   (declare (fixnum integer count))
   (ash integer (- count)))
+
+(declaim (inline range))
+(defun range (from to &optional (step 1))
+  (let ((step (if (> from to)
+                  (- step)
+                  step))
+        (test (if (> from to)
+                  (lambda (i to) (<= i to))
+                  (lambda (i to) (>= i to)))))
+    (do ((i from (+ i step))
+         (result (list) (push i result)))
+        ((funcall test i to) (nreverse result)))))
--- src/vector.lisp
+++ src/vector.lisp
@@ -1,16 +1,21 @@
 (cl:defpackage #:persistine.vector
-  (:use #:cl #:alexandria #:optima #:persistine.utils))
+  (:use #:cl #:alexandria #:optima #:persistine.utils)
+  (:export
+   #:make-persistent-vector
+   #:pvector-p
+   #:pvector-ref
+   #:pvector-extend))
 (cl:in-package #:persistine.vector)
 
 (declaim (optimize (speed 3)))
 
 (defstruct (persistent-vector (:constructor %make-persistent-vector)
                               (:conc-name :pv-)
-                              (:predicate pv-p))
+                              (:predicate :pvector-p))
   root
   tail
-  length
-  height)
+  (length 0 :type fixnum)
+  (height 1 :type fixnum))
 
 (defun make-persistent-vector (&key root tail (length 0) (height 1))
   (let ((root (or root (make-node)))
@@ -20,6 +25,62 @@
                              :length length
                              :height height)))
 
+(defun pvector-ref (vector index)
+  (node-get (get-node vector index)
+            (index-at-height index 0)))
+
+(defun pvector-extend (vector item)
+  (let ((root (pv-root vector))
+        (tail (pv-tail vector))
+        (length (pv-length vector))
+        (height (pv-height vector))
+        (tail-offset (tail-offset vector)))
+    (cond
+      ((< (- length tail-offset) 32) ; Space in tail
+       (let ((new-tail (node-push tail item)))
+         (make-persistent-vector :root root
+                                 :tail new-tail
+                                 :length (1+ length)
+                                 :height height)))
+      ((> (rsh length 5) (capacity height)) ; No space left in root
+       (let ((new-root (make-node root (node-grow tail height))))
+         (make-persistent-vector :root new-root
+                                 :tail (make-node item)
+                                 :length (1+ length)
+                                 :height (1+ height))))
+      (t
+       (make-persistent-vector :root (node-insert root tail length height)
+                               :tail (make-node item)
+                               :length (1+ length)
+                               :height height)))))
+
+(defun index-at-height (index height)
+  (declare (fixnum index height))
+  (logand (rsh index (* height 5)) 31))
+
+(defun capacity (height)
+  (declare (fixnum height))
+  (ash 1 (* height 5)))
+
+(defun tail-offset (vector)
+  (let ((length (pv-length vector)))
+    (if (< length 32)
+        0
+        (ash (rsh (1- length) 5) 5))))
+
+(defun get-node (vector index)
+  "Returns node that holds item of given index"
+  (declare (fixnum index))
+  (let ((height (pv-height vector))
+        (tail-offset (tail-offset vector)))
+    (if (>= index tail-offset)
+        (pv-tail vector)
+        (reduce (lambda (node height)
+                  (node-get node
+                            (index-at-height index height)))
+                (range height 0)
+                :initial-value (pv-root vector)))))
+
 (defun make-node (&rest items)
   (let ((array (make-32-array)))
     (dolist (item items array)
@@ -28,69 +89,34 @@
 (defun copy-node (node)
   (copy-array node))
 
-(defun tail-offset (vector)
-  (match vector
-    ((pv- length)
-     (declare (fixnum length))
-     (if (< length 32)
-         0
-         (ash (rsh (1- length) 5) 5)))))
-
-(defun descent (vector index)
-  "Returns the node of the tree at whose level the given index is found"
+(defun node-get (node index)
   (declare (fixnum index))
-  (match vector
-    ((pv- root tail height)
-     (if (>= index (tail-offset vector))
-         tail
-         (do* ((shift (* height 5)
-                      (- shift 5))
-               (node root
-                     (aref node (logand (rsh index shift)
-                                        31))))
-              ((<= shift 0) node))))))
+  (aref node index))
 
-(defun pvector-ref (vector index)
-  (aref (descent vector index)
-        (logand index 31)))
+(defun node-set (node index value)
+  (declare (fixnum index))
+  (let ((new-node (copy-node node)))
+    (setf (aref new-node index)
+          value)
+    new-node))
 
-(defun pvector-cons (vector item)
-  (match vector
-    ((pv- root tail length height)
-     (cond
-       ((< (- length (tail-offset vector)) 32)
-        (let ((new-tail (copy-node tail)))
-          (vector-push item new-tail)
-          (make-persistent-vector :root root
-                                  :tail new-tail
-                                  :length (1+ length)
-                                  :height height)))
-       ((> (rsh length 5) (ash 1 (* height 5)))
-        (let ((new-root (make-node)))
-          (vector-push root new-root)
-          (vector-push (expand-to-height tail height) new-root)
-          (make-persistent-vector :root new-root
-                                  :tail (make-node item)
-                                  :length (1+ length)
-                                  :height (1+ height))))
-       (t
-        (make-persistent-vector :root (insert-tail root tail length height)
-                                :tail (make-node item)
-                                :length (1+ length)
-                                :height height))))))
+(defun node-push (node value)
+  (let ((new-node (copy-node node)))
+    (vector-push value new-node)
+    new-node))
 
-(defun expand-to-height (node height)
+(defun node-grow (node height)
+  (declare (fixnum height))
   (if (= height 0)
       node
-      (make-node (expand-to-height node (- height 1)))))
+      (make-node (node-grow node (1- height)))))
 
-(defun insert-tail (node item length height)
-  (let* ((new-node (copy-node node))
-         (next-index (logand (rsh (1- length) (* 5 height)) 31))
-         (child (aref new-node next-index)))
-    (if child
-        (setf (aref new-node next-index)
-              (insert-tail child item length (1- height)))
-        (vector-push (expand-to-height item (1- height))
-                     new-node))
-    new-node))
+(defun node-insert (node item length height)
+  (declare (fixnum length height))
+  (if (= height 1)
+      (node-push node item)
+      (let ((next-index (index-at-height (1- length) height)))
+        (if-let (child (node-get node next-index))
+          (node-set node next-index
+                    (node-insert child item length (1- height)))
+          (node-push node (node-grow item (1- height)))))))
